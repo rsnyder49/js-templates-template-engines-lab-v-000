2cafbdc6d9715a047e26648eab2d1fb2017b86e7
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 23:33:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function createPost() {  
   let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   let postTemplate = _.template(document.getElementById("post-template").innerHTML);
-  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
-  var title = document.getElementById("postTitle").value;
-  var body = document.getElementById("postBody").value;
-  var author = document.getElementById("postAuthor").value;
+  let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+  let title = document.getElementById("postTitle").value;
+  let body = document.getElementById("postBody").value;
+  let author = document.getElementById("postAuthor").value;
  
   document.getElementById("post-form").innerHTML += pageTemplate();
  
-  var blog = postTemplate({ 'title': title, 'body': body, 'author': author });
-  var comments = commentsTemplate();
-  var post = document.getElementById("post");
+  let blog = postTemplate({ 'title': title, 'body': body, 'author': author });
+  let comments = commentsTemplate();
+  let post = document.getElementById("post");
  
   post.innerHTML = blog;
   post.getElementsByTagName("footer")[0].innerHTML = comments;
 }
+
+function postComment() {
+  let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+  let name = document.getElementById("commenter").value;
+  let comment = document.getElementById("commentBody").value;
+  let commentsSection = document.getElementById("comments");
+  
+  commentsSection.innerHTML += commentTemplate({ 'commenter': name, 'comment': comment });
+} 
+
+
